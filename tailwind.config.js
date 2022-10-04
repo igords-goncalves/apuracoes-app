f7a28d3780edcee81d3decb31f9be5888210cdaa
Igor Gonçalves <igordossantos38@gmail.com>
2022-10-04 19:27:35 -0300

    fix bug on tailwind
    Fixing bug on tailwind installation for React

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js, jsx, ts, tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
