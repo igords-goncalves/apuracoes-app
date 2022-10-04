ce549e06505cbcc55483de657126d759c1441948
Igor Gonçalves <igordossantos38@gmail.com>
2022-10-04 20:23:19 -0300

    stylish
    Giving style on header and changing font family on global css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,11 @@
 function Header() {
     return (
         <div className="w-full h-80 bg-green-600">
-            <span className="text-5xl text-white">Teste</span>
+            <div className="w-2/4">
+                <h1 className="text-5xl font-bold text-white">Eleiçoes 2022</h1>
+                <h2 className=" text-4xl text-white">Geral Ordinária</h2>
+                <h3 className="text-2xl font-black">02/10/2022</h3>
+            </div>
         </div>
     )
 }
