3646858552a617104b4013c51991e7bf9ff3d5a0
Igor Gonçalves <igordossantos38@gmail.com>
2022-10-04 20:38:14 -0300

    stylish
    Giving style on header elements title

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,20 @@
 function Header() {
     return (
-        <div className="w-full h-80 bg-green-600">
-            <div className="w-2/4">
-                <h1 className="text-5xl font-bold text-white">Eleiçoes 2022</h1>
-                <h2 className=" text-4xl text-white">Geral Ordinária</h2>
-                <h3 className="text-2xl font-black">02/10/2022</h3>
+        <div className="w-full h-80 bg-green-500">
+
+            <div className="pt-10 pl-10">
+                <h1 className="text-5xl font-bold text-white mb-3">
+                    Eleiçoes 2022
+                </h1>
+                <h2 className=" text-4xl font-light text-white mb-3">
+                    Geral Ordinária
+                </h2>
+                <h3 className="text-2xl font-black text-green-700">
+                    02/10/2022
+                </h3>
             </div>
         </div>
-    )
+    );
 }
 
-export default Header
+export default Header;
