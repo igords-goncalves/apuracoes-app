aaafc96757c204add99b631628605111be9af0a7
Igor Gonçalves <igordossantos38@gmail.com>
2022-10-05 14:15:29 -0300

    manipulate array
    Advancing in manipulate data response of fetch

--- src/components/main/data/Data.jsx
+++ src/components/main/data/Data.jsx
@@ -1,39 +1,42 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 function Data(props) {
+    const url =
+        "https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json";
 
-    const url = 'https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json'
-
-    const [id, setId] = useState(0)
-    const [votes, setVotes] = useState(0)
-    const [percentVotes, setPercentVotes] = useState(0)
-    const [party, setParty] = useState("")
-    const [number, setNumber] = useState(0)
-    const [name, setName] = useState("")
+    const [id, setId] = useState(0);
+    const [votes, setVotes] = useState(0);
+    const [percentOfVotes, setPercentOfVotes] = useState(0);
+    const [party, setParty] = useState("");
+    const [number, setNumber] = useState(0);
+    const [name, setName] = useState("");
 
     useEffect(() => {
         fetch(url)
-            .then(res => res.json())
-            .then(data => {
-                data.cand.forEach(data => {
-                    setId(data.seq)
-                    setVotes(data.vap)
-                    setPercentVotes(data.pvap)
-                    setParty(data.cc)
-                    setName(data.nm)
-                    setNumber(data.n)
-                });
+            .then((res) => res.json())
+            .then((data) => {
+                const candidates = data.cand;
+
+                return candidates.map((obj) => {
+                    return {
+                        id: obj.seq,
+                        votes: obj.vap,
+                        percentOfVotes: obj.pvap,
+                        party: obj.cc,
+                        number: obj.n,
+                        name: obj.nm,
+                    }
+                })
             })
-    }, [])
+            // console.log(datas[0].name)
+            .then(datas => console.log(datas[0].name))
+    }, []);
 
     return (
         <div>
-            <p>{votes}</p>
-            <p>{percentVotes}</p>
-            <p>{party}</p>
             <p>{name}</p>
         </div>
-    )
+    );
 }
 
-export default Data
+export default Data;
