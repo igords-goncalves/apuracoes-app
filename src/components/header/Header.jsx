820ec62f19bca7a77d5c9b3389ef552f598ca005
Igor Gonçalves <igordossantos38@gmail.com>
2022-10-04 23:15:57 -0300

    colors and size
    Using colors planed, and changing size's font and header height

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,15 +2,15 @@ import Button from "../button/Button";
 
 function Header() {
     return (
-        <div className="w-full h-80 bg-green-500 flex items-center justify-between">
+        <div className="w-full h-64 bg-[#158A64] flex items-center justify-between">
             <div className="ml-16">
-                <h1 className="text-5xl font-bold text-white mb-3">
+                <h1 className="text-4xl font-bold text-white mb-3">
                     Eleiçoes 2022
                 </h1>
-                <h2 className=" text-4xl font-light text-white mb-3">
+                <h2 className=" text-2xl font-light text-white mb-3">
                     Geral Ordinária
                 </h2>
-                <h3 className="text-2xl font-black text-green-700">
+                <h3 className="text-xl font-black text-[#177353]">
                     02/10/2022
                 </h3>
             </div>
@@ -20,7 +20,7 @@ function Header() {
                 mr-16 
                 rounded-full 
                 border-none 
-                bg-green-700 
+                bg-[#177353] 
                 px-10 
                 py-3 
                 text-3xl 
