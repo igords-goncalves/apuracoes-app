9150679d0b38fd9d28821f4e19d8078b362be207
Igor Gonçalves <igordossantos38@gmail.com>
2022-10-05 12:39:39 -0300

    datas
    Making a request and trying to render all candidates trough forEach
    function

--- src/components/main/data/Data.jsx
+++ src/components/main/data/Data.jsx
@@ -4,9 +4,11 @@ function Data(props) {
 
     const url = 'https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json'
 
+    const [id, setId] = useState(0)
     const [votes, setVotes] = useState(0)
     const [percentVotes, setPercentVotes] = useState(0)
     const [party, setParty] = useState("")
+    const [number, setNumber] = useState(0)
     const [name, setName] = useState("")
 
     useEffect(() => {
@@ -14,16 +16,22 @@ function Data(props) {
             .then(res => res.json())
             .then(data => {
                 data.cand.forEach(data => {
-                    console.log(data.nm)
+                    setId(data.seq)
+                    setVotes(data.vap)
+                    setPercentVotes(data.pvap)
+                    setParty(data.cc)
+                    setName(data.nm)
+                    setNumber(data.n)
                 });
             })
     }, [])
 
-
-
     return (
         <div>
-            Data
+            <p>{votes}</p>
+            <p>{percentVotes}</p>
+            <p>{party}</p>
+            <p>{name}</p>
         </div>
     )
 }
