23c9c49b9fc1e431d9c204b7fcdd2f22fc0a4251
Igor Gonçalves <igordossantos38@gmail.com>
2022-10-05 11:59:40 -0300

    images and responsivity
    Referenced images and make it reponsive

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,9 +1,25 @@
 import Image from "./image/Image";
 
-function Main(props) {
+import candidateA from  '../../assets/img/ciro.png'
+import candidateB from  '../../assets/img/bolsonaro.png'
+import candidateC from  '../../assets/img/lula.png'
+import candidateD from  '../../assets/img/simone.png'
+import candidateE from  '../../assets/img/soraya.png'
+
+function Main() {
     return (
-        <main>
-            <Image />
+        <main className="
+            flex 
+            mx-8 my-8
+            flex-col
+            items-center
+            md:flex-row
+            ">
+            <Image img={candidateA}/>
+            <Image img={candidateB}/>
+            <Image img={candidateC}/>
+            <Image img={candidateD}/>
+            <Image img={candidateE}/>
         </main>
     )
 }
