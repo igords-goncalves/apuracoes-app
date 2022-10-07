21749a09e5f812cdda79e7503ec1faf2d440730b
Igor Gonçalves <igordossantos38@gmail.com>
2022-10-07 16:22:28 -0300

    organize code
    Organizing code

--- src/components/main/data/Data.jsx
+++ src/components/main/data/Data.jsx
@@ -2,10 +2,18 @@ function Data(props) {
     return (
         <div className="px-10">
             <ul>
-                <li className="font-normal text-sm text-gray-900">{props.votes}</li>
-                <li className="font-semibold text-2xl text-gray-800">{props.percent}%</li>
-                <li className="font-light text-base text-gray-900">PT</li>
-                <li className="font-semibold text-sm text-gray-900">{props.number} - {props.name}</li>
+                <li className="font-normal text-sm text-gray-900">
+                    {props.votes}
+                </li>
+                <li className="font-semibold text-2xl text-gray-800">
+                    {props.percent}%
+                </li>
+                <li className="font-light text-base text-gray-900">
+                    PT
+                </li>
+                <li className="font-semibold text-sm text-gray-900">
+                    {props.number} - {props.name}
+                </li>
             </ul>
         </div>
     );
