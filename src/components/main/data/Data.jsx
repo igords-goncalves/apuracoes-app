c3d0943b19e0631af9ecd07dc9b91c24ae44e7f6
Igor Gonçalves <igordossantos38@gmail.com>
2022-10-07 15:25:44 -0300

    render datas and images
    It works making datas render in display and images like a card, logic
    is in Card component

--- src/components/main/data/Data.jsx
+++ src/components/main/data/Data.jsx
@@ -1,43 +1,11 @@
-import { useEffect, useState } from "react";
-
 function Data(props) {
-
-    const [candidateA, setCandidateA] = useState({});
-    const [candidateB, setCandidateB] = useState({});
-    const [candidateC, setCandidateC] = useState({});
-    const [candidateD, setCandidateD] = useState({});
-    const [candidateE, setCandidateE] = useState({});
-
-    const url =
-        "https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json";
-
-    const [candidates, setCandidates] = useState([]);
-
-    useEffect(() => {
-        fetch(url)
-            .then((res) => res.json())
-            .then((data) => {
-                return setCandidates(data.cand);
-            });
-    }, []);
-
-
-    useEffect(() => {
-        candidates.map((curr, i, arr) => {
-            setCandidateA(arr[0])
-            setCandidateB(arr[1])
-            setCandidateC(arr[2])
-            setCandidateE(arr[3])
-            setCandidateD(arr[4])
-        })
-    })
-
-    console.log(candidateA, candidateB, candidateC, candidateD, candidateE)
-
     return (
-        <div>
+        <div className="px-8">
             <ul>
-                <li></li>
+                <li>{props.votes}</li>
+                <li>{props.percent}</li>
+                <li>PT</li>
+                <li className="flex ">{props.number} - {props.name}</li>
             </ul>
         </div>
     );
