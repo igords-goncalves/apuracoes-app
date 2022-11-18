476fcdea1255104f92482108d952b311558e212d
Igor Gonçalves <igordossantos38@gmail.com>
2022-11-18 18:15:24 -0300

    design refactor
    Removed button "Atualizar" not necessary to the app

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,15 +1,9 @@
 import { Fade } from "react-awesome-reveal";
 
-import Button from "../button/Button";
 import Card from "./data/Card";
 import CardSecondRound from "./data/CardSecondRound";
 
 function Main(props) {
-
-    function refreshPage() {
-        window.location.reload(true)
-    }
-
     function getRound(label) {
         if(label === "1º TURNO") {
             return (
@@ -38,23 +32,6 @@ function Main(props) {
                     {getRound(props.label)}
                 </main>
             </Fade>
-
-            <div className="flex items-center justify-center">
-                <Button
-                    title="Atualizar"
-                    mt="mt-8 md:mt-0"
-                    option="
-                rounded-full 
-                border-none 
-                bg-[#FEB034]
-                px-10 
-                py-3 
-                text-2xl md:text-3xl 
-                font-medium 
-                text-white"
-                    callBack={() => refreshPage()}
-                />
-            </div>
         </>
     );
 }
