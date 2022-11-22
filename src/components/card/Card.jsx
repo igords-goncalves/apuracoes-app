43363a2590825b503b5b23c3adb81ae2fe692481
Igor Gonçalves <igordossantos38@gmail.com>
2022-11-22 09:54:35 -0300

    refact code
    Refactoring code and creating a env file for apis consumming

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,150 @@
+import Image from "../image/Image";
+
+import candidateA from "../../assets/img/lula.png";
+import candidateB from "../../assets/img/bolsonaro.png";
+import candidateC from "../../assets/img/simone.png";
+import candidateD from "../../assets/img/ciro.png";
+import candidateE from "../../assets/img/soraya.png";
+
+import Data from "../data/Data";
+
+import { useEffect, useState } from "react";
+
+function Card() {
+    const [candidate0, setCandidate0] = useState({});
+    const [candidate1, setCandidate1] = useState({});
+    const [candidate2, setCandidate2] = useState({});
+    const [candidate3, setCandidate3] = useState({});
+    const [candidate4, setCandidate4] = useState({});
+
+    const baseURL = process.env.REACT_APP_TSE1ROUNDAPI
+
+    const [candidates, setCandidates] = useState([]);
+
+
+    function fetchData(url) {
+        fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+            return setCandidates(data.cand);
+        });
+    }
+
+    function getCandidates() {
+        candidates.map((curr, i, arr) => {
+            setCandidate0(arr[0]);
+            setCandidate1(arr[1]);
+            setCandidate2(arr[2]);
+            setCandidate3(arr[3]);
+            setCandidate4(arr[4]);
+            return null
+        });
+    }
+    
+    function getParty(string) {
+        try {
+            if(!string) return
+            return string.split('-')[0]
+        } catch (error) {
+            throw error
+        }
+    }
+
+    useEffect(() => fetchData(baseURL), []);
+
+    useEffect(() => getCandidates());
+
+
+    return (
+        <>
+            <div
+                className="
+            flex
+            flex-col
+            mb-10
+            md:mx-10
+            "
+            >
+                <Image img={candidateA} />
+                <Data
+                    votes={candidate0.vap}
+                    percent={candidate0.pvap}
+                    party={getParty(candidate0.cc)}
+                    number={candidate0.n}
+                    name={candidate0.nm}
+                />
+            </div>
+            <div
+                className="
+            flex
+            flex-col
+            mb-10
+            md:mx-10
+            
+            "
+            >
+                <Image img={candidateB} />
+                <Data
+                    votes={candidate1.vap}
+                    percent={candidate1.pvap}
+                    party={getParty(candidate1.cc)}
+                    number={candidate1.n}
+                    name={candidate1.nm}
+                />
+            </div>
+            <div
+                className="
+            flex
+            flex-col
+            mb-10
+            md:mx-10
+            "
+            >
+                <Image img={candidateC} />
+                <Data
+                    votes={candidate2.vap}
+                    percent={candidate2.pvap}
+                    party={getParty(candidate2.cc)}
+                    number={candidate2.n}
+                    name={candidate2.nm}
+                />
+            </div>
+            <div
+                className="
+            flex
+            flex-col
+            mb-10
+            md:mx-10
+            "
+            >
+                <Image img={candidateD} />
+                <Data
+                    votes={candidate3.vap}
+                    percent={candidate3.pvap}
+                    party={getParty(candidate3.cc)}
+                    number={candidate3.n}
+                    name={candidate3.nm}
+                />
+            </div>
+            <div
+                className="
+            flex
+            flex-col
+            mb-10
+            md:mx-10
+            "
+            >
+                <Image img={candidateE} />
+                <Data
+                    votes={candidate4.vap}
+                    percent={candidate4.pvap}
+                    party={getParty(candidate4.cc)}
+                    number={candidate4.n}
+                    name={candidate4.nm}
+                />
+            </div>
+        </>
+    );
+}
+
+export default Card;
